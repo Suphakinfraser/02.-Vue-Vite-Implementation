f34123f08e952fbf5fc4ec1fb985debe38e58256
tommyyeebai <115710017+tommyyeebai@users.noreply.github.com>
2023-08-10 20:32:20 +0700

    step 4.14

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -43,8 +43,15 @@ const events = ref<EventItem[]>([
 </script>
 
 <template>
-  <main>
-    <!-- <TheWelcome /> -->
+  <main class="events">
+        <!-- <TheWelcome /> -->
     <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
 </main>
 </template>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
